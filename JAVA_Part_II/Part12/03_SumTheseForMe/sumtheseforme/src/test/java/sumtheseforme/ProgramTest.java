912f1da04d53fe5d8ce366c1f7db4d6ecb771d96
PortugueseWolf <danieljdferreira@gmail.com>
2024-07-07 17:51:36 +0100

    Done

---
+++ JAVA_Part_II/Part12/03_SumTheseForMe/sumtheseforme/src/test/java/sumtheseforme/ProgramTest.java
@@ -0,0 +1,12 @@
+package sumtheseforme;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ProgramTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
