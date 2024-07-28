33d3ccd9a248ab58b8ad12e7e10dbaa6b843e9b4
PortugueseWolf <danieljdferreira@gmail.com>
2024-07-28 15:28:29 +0100

    Done

---
+++ JAVA_Part_II/Part13/03_ButtonAndTextField/README.md
@@ -0,0 +1,6 @@
+
+# Button and TextField
+
+Create a graphical user interface to the class in the exercise template. The interface must contain a button and a text field. The text field can be implemented using the TextField class. The Button must be on the left or on the top of the text field.
+
+Create a graphical user interface in the class that is supplied with the exercise base. It should contain a button and a text field. You can implement the text field with the help of the TextField class. The button should be placed above or to the left of the text field.
---
+++ JAVA_Part_II/Part13/03_ButtonAndTextField/buttonandtextfield/src/main/java/buttonandtextfield/ButtonAndTextFieldApplication.java
@@ -0,0 +1,33 @@
+package buttonandtextfield;
+
+import javafx.application.Application;
+import javafx.geometry.Orientation;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class ButtonAndTextFieldApplication extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        Button button = new Button("New button");
+        TextField text = new TextField("Lorem ipsum");
+
+        FlowPane pane = new FlowPane(Orientation.VERTICAL);
+
+        pane.getChildren().add(button);
+        pane.getChildren().add(text);
+
+        Scene scene = new Scene(pane);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
